03e086ca642c4746fea0ee0f2f0db57fd014b19a
vinay <vinay@gmail.com>
2024-02-25 13:45:31 +0530

    First Commit

--- hacki/hacki/asgi.py
+++ hacki/hacki/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hacka project.
+ASGI config for hacki project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hacka.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hacki.settings')
 
 application = get_asgi_application()
--- hacki/hacki/wsgi.py
+++ hacki/hacki/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hacka project.
+WSGI config for hacki project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hacka.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hacki.settings')
 
 application = get_wsgi_application()
